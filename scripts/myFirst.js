7233c838ea7720b67d4b2c38b7f686b41a1be638
arabaykaa <56497790+arabaykaa@users.noreply.github.com>
2022-03-13 22:37:57 +0600

    Hi!

--- scripts/myFirst.js
+++ scripts/myFirst.js
@@ -1,20 +1,3 @@
 "use strict";
 
-function Calculator() {
-  this.calculate = function (str) {
-    let mas = str.split(" ");
-    for (let i = 0; i < mas.length; i++) {
-      let res;
-      if (mas[i] == "+") {
-        return (res = +mas[0] + +mas[mas.length - 1]);
-      } else if (mas[i] == "-") {
-        return (res = +mas[0] - +mas[mas.length - 1]);
-      }
-    }
-    return res;
-  };
-}
-
-let calc = new Calculator();
-
-let str = "1 + 5";
+alert("Hello World!");
