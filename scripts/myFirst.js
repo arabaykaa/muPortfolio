5faaac7c7cf668724e20e8248cf0a32ca3a9693c
arabaykaa <56497790+arabaykaa@users.noreply.github.com>
2022-03-21 15:56:08 +0600

    Class pracits JS

--- scripts/myFirst.js
+++ scripts/myFirst.js
@@ -1,23 +1,48 @@
 "use strict";
 
-let cat = {
-  head: [],
-  wear(hat) {
-    this.head = hat;
-  },
-};
+// let cat = {
+//   head: [],
+//   wear(hat) {
+//     this.head = hat;
+//   },
+// };
 
-let white = {
-  __proto__: cat,
-};
+// let white = {
+//   __proto__: cat,
+// };
 
-let black = {
-  __proto__: cat,
-};
+// let black = {
+//   __proto__: cat,
+// };
 
+// white.wear('Shapka');
+// console.log(white.head);
 
-white.wear('Shapka');
-console.log(white.head);
+// console.log(black.head);
 
+class User {
+  constructor(name, secondname) {
+    this.name = name;
+    this.secondname = secondname;
+  }
+  printName() {
+    console.log(this.name, this.secondname);
+  }
+}
 
-console.log(black.head);
+class Worker extends User{
+  constructor(name, secondname, salary){
+    super(name, secondname);
+    this.salary = salary;
+  }
+  salaryTax(){
+    console.log(this.salary * 0.8);
+  }
+}
+
+let martin = new User("Martin", "Jhonson");
+martin.printName();
+
+let newWorker = new Worker("Sam", "Smith", 3000);
+newWorker.salaryTax();
+newWorker.printName();
